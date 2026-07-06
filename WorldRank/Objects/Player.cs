0d88ce0897f4a66116a080681e26127b8f00551d
Κριστιαν Σοτιρι <kristiansotiri1@gmail.com>
2026-07-06 19:38:31 +0300

    Updated Class Player

---
+++ WorldRank/Objects/Player.cs
@@ -0,0 +1,93 @@
+﻿namespace WorldRank.Objects
+{
+    public class Player(string name, int score = 0) // Primary Constructor
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; } = name;
+        public int Score { get; private set; } = score;
+
+
+        public static void AddPlayer(List<Player> player)
+        {
+            string? nameInput = null;
+
+            while (string.IsNullOrWhiteSpace(nameInput))
+            {
+                Console.Write("Enter your name: ");
+                nameInput = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(nameInput))
+                {
+                    Console.WriteLine("Name is required !");
+                }
+            }
+
+            Player p = new(nameInput);
+            player.Add(p);
+
+            Console.WriteLine($"Player {nameInput} is added !");
+
+        }
+
+        public static void ListPlayers(List<Player> players)
+        {
+            if (players.Count == 0)
+            {
+                Console.WriteLine($"No Players found! ");
+                return;
+            }
+
+            foreach (Player p in players)
+            {
+                Console.WriteLine($"{p.Name} - Score: {p.Score}");
+            }
+        }
+
+        public static void FindPlayerByName(List<Player> players)
+        {
+            Console.WriteLine("Enter name: ");
+            string? input = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Name is required.");
+                return;
+            }
+
+            //Player? player = players.FirstOrDefault(p => p.Name.Equals(input, StringComparison.OrdinalIgnoreCase));
+
+            List<Player>? foundPlayers = players.Where(p => p.Name.Equals(input, StringComparison.OrdinalIgnoreCase)).ToList();
+
+
+            //if (player == null)
+            //{
+            //    Console.WriteLine("Player not found");
+            //    return;
+            //}
+
+            if (foundPlayers.Count == 0)
+            {
+                Console.WriteLine("Player not found.");
+                return;
+            }
+
+            foreach (Player player in foundPlayers)
+            {
+                Console.WriteLine($"{player.Name} - Score: {player.Score}");
+            }
+        }
+
+        public static void UpdateScore(Player player, int score)
+        {
+            if (player == null)
+            {
+                Console.WriteLine("Player not found");
+                return;
+            }
+
+            player.Score += score;
+
+            Console.WriteLine($"Player Score is updated to: {player.Score}");
+        }
+    }
+}
--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using WorldRank.Objects;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Player> players = new List<Player>();
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("1. Add Player");
+            Console.WriteLine("2. List Players");
+            Console.WriteLine("3. Search Player");
+            string? input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int choice))
+            {
+                Console.WriteLine("Please enter a valid number!");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Player.AddPlayer(players);
+                    break;
+
+                case 2:
+                    Player.ListPlayers(players);
+                    break;
+
+
+                case 3:
+                    Player.FindPlayerByName(players);
+                    break;
+            }
+        }
+    }
+}
