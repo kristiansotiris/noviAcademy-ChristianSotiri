23e461b0970150261b1dbabd01a8ef994d77a48f
christian <User@DESKTOP-FS5J75R>
2026-07-07 16:00:32 +0300

    intialize changes interfaces to classes

---
+++ WorldRank/Objects/InMemoryWalletRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorldRank.Interfaces;
+
+namespace WorldRank.Objects
+{
+    public class InMemoryWalletRepository : IWalletRepository
+    {
+        public void AddWallet(Wallet wallet, int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetWalletsByPlayer(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
