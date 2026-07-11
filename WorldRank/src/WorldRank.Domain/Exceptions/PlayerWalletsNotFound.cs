3f4da927a194a7572b63c23f78ec21625ed4ddcc
Κριστιαν Σοτιρι <kristiansotiri1@gmail.com>
2026-07-11 15:31:41 +0300

    initialized changes added dbcontext and updated functions

---
+++ WorldRank/src/WorldRank.Domain/Exceptions/PlayerWalletsNotFound.cs
@@ -0,0 +1,26 @@
+﻿using WorldRank.src.WorldRank.Domain.Entities;
+using WorldRank.src.WorldRank.Domain.Enums;
+using WorldRank.src.WorldRank.Domain.Exceptions;
+
+namespace WorldRank.Domain.Exceptions
+{
+    public sealed class PlayerWalletsNotFound : WalletException
+    {
+        public int PlayerId { get; }
+        public Currency Currency { get; }
+
+        public PlayerWalletsNotFound(Currency currency, int playerId) : base($"No {currency} wallet found for player {playerId}.")
+        {
+            PlayerId = playerId;
+            Currency = currency;
+        }
+
+        public PlayerWalletsNotFound(int playerId) : base($"No wallet found for player {playerId}.")
+        {
+            PlayerId = playerId;
+        }
+
+        public PlayerWalletsNotFound(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
---
+++ WorldRank/src/WorldRank.Infrastructure/Contexts/WorldRankDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldRank.src.WorldRank.Domain.Entities;
+
+namespace WorldRank.Infrastructure.Contexts
+{
+    public class WorldRankDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
