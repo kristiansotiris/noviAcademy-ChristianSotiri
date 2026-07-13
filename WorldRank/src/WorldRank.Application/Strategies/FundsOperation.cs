1601a160c0da603a1b9852469e35bd5794634e71
Κριστιαν Σοτιρι <kristiansotiri1@gmail.com>
2026-07-13 22:30:40 +0300

    intialized changes updated project structure

--- WorldRank/src/WorldRank.Application/Strategies/FundsOperation.cs
+++
@@ -1,8 +0,0 @@
-namespace WorldRank.src.WorldRank.Application.Strategies;
-
-public enum FundsOperation
-{
-    Add,
-    Subtract,
-    ForceSubtract
-}
--- WorldRank/src/WorldRank.Domain/Exceptions/WorldRankException.cs
+++
@@ -1,7 +0,0 @@
-namespace WorldRank.src.WorldRank.Domain.Exceptions;
-
-public class WorldRankException : Exception
-{
-    public WorldRankException(string message) : base(message) { }
-    public WorldRankException(string message, Exception inner) : base(message, inner) { }
-}
