ebf421726e5a82ee2ae2dfb340317f9f7bb8dfd9
christian <User@DESKTOP-FS5J75R>
2026-07-15 15:35:36 +0300

    initialized changes !

--- NoviCode.Gateway/EcbHTTPcLIENT.cs
+++ NoviCode.Gateway/EcbHTTPcLIENT.cs
@@ -1,13 +1,28 @@
 ﻿using NoviCode;
 using NoviCode.Dtos;
+using System.Xml.Serialization;
 
 namespace Novicode.Gateway
 {
     public class EcbHttpClient : IEcbHttpClient
     {
-        public Task<IReadOnlyList<CurrencyRateDto>> GetLatestRatesAsync(CancellationToken cancellationToken = default)
+        private readonly HttpClient _httpClient;
+
+        public EcbHttpClient(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<IReadOnlyList<CurrencyRateDto>> GetLatestRatesAsync(CancellationToken cancellationToken = default)
+        {
+           var response = await _httpClient.GetAsync("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+
+           var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+           var serializer = new XmlSerializer(typeof(CurrencyRateDto));
+
+           var responseDto = serializer.Deserialize(stream);
+
         }
     }
 }
